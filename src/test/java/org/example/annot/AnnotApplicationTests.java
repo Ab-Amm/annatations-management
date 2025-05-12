7f39e3ca1d40459622b2054b3b9a3b6c2c9a80cb
Ab-Amm <ab.amm209@gmail.com>
2025-05-12 19:49:54 +0100

    initial commit

---
+++ src/test/java/org/example/annot/AnnotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.annot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
